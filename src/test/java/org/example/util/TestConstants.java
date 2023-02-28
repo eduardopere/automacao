e204dacf497768673d5c9898a79603ff5bad26d9
Eduardo Pereira <eduardopereira21@gmail.com>
2023-02-28 20:07:31 -0300

    update readme

--- src/test/java/org/example/util/TestConstants.java
+++ src/test/java/org/example/util/TestConstants.java
@@ -3,5 +3,6 @@ package org.example.util;
 public class TestConstants {
 
     public static final String DEFAULT_URL = "https://reqres.in/api/users";
+    public static final String DEFAULT_URL_GET = "https://reqres.in/api/users?page=2";
 
 }
